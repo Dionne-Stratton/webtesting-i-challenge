6a2f920a9c63ca554d93bc140e37cc51a978d877
Dionne Stratton <mylife33@hotmail.com>
2020-12-16 18:58:03 -0600

    init

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,25 @@ module.exports = {
 };
 
 function success(item) {
-  return { ...item };
+  if (item.level === 20) {
+    return item;
+  } else {
+  return { ...item, level: item.level + 1 };
+}
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.level < 15) {
+    return { ...item, durability: item.durability - 5 };
+  } else if (item.level === 15) {
+    return { ...item, durability: item.durability - 10 };
+  } else {
+    return { ...item, durability: item.durability - 10, level: item.level - 1 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
